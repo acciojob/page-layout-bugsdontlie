9842715662e2fc742b496a298407c71931ae2c5e
Vaibhav <vaibhav_accio@acciojob.com>
2026-01-29 11:38:08 +0530

    Fix formatting of paragraph and footer elements

--- src/components/App.js
+++ src/components/App.js
@@ -8,8 +8,8 @@ const App = () => {
       {/* Do not remove the main div */}
       <PageLayout>
         <h1>Welcome to my website</h1>
-        <p>This is the content of my website</p>
-        <footer>© 2023 My Website. All rights reserved. </footer>
+        <p>This is the content of my website.</p>
+        <footer>© 2023 My Website. All rights reserved.</footer>
       </PageLayout>
     </div>
   );
