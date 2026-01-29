4743d9938ab7015ba2ff93869fb95be78d82bf94
Vaibhav <vaibhav_accio@acciojob.com>
2026-01-29 11:35:50 +0530

    Fix capitalization in welcome message

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ const App = () => {
     <div>
       {/* Do not remove the main div */}
       <PageLayout>
-        <h1>Welcome to my Website</h1>
+        <h1>Welcome to my website</h1>
         <p>This is the content of my website</p>
         <footer>© 2023 My Website. All rights reserved. </footer>
       </PageLayout>
